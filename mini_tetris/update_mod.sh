a01713f9e3aaea6ee0bfc7db9ff9bada1df9cdf7
Jubin Park <jubin-park@naver.com>
2021-05-25 02:17:18 +0900

    add: echo message in update_mod.sh

--- mini_tetris/update_mod.sh
+++ mini_tetris/update_mod.sh
@@ -1,7 +1,11 @@
 DRIVER_DIR="drivers"
 
+echo "Update FPGA module drivers ..."
+
 sudo rmmod dot_matrix_driver 
 sudo insmod ./${DRIVER_DIR}/dot_matrix_driver.ko
 
 sudo rmmod led_driver
 sudo insmod ./${DRIVER_DIR}/led_driver.ko
+
+sudo lsmod
