8ed5873d00b68f3524a98c40e83f3093d2ff0001
Jubin Park <jubin-park@naver.com>
2021-05-25 03:55:18 +0900

    update: update_mod.sh

--- mini_tetris/update_mod.sh
+++ mini_tetris/update_mod.sh
@@ -2,29 +2,25 @@ DRIVER_DIR="drivers"
 
 echo "Update FPGA module drivers ..."
 
-sudo -s
+sudo mknod /dev/csemad_led c 260 0
+sudo mknod /dev/csemad_seven_segment c 261 0
+sudo mknod /dev/csemad_dot_matrix c 262 0
+sudo mknod /dev/csemad_lcd_text c 263 0
+sudo mknod /dev/csemad_buzzer c 264 0
+sudo mknod /dev/csemad_push_switch c 265 0
 
-mknod /dev/csemad_led c 260 0
-mknod /dev/csemad_seven_segment c 261 0
-mknod /dev/csemad_dot_matrix c 262 0
-mknod /dev/csemad_lcd_text c 263 0
-mknod /dev/csemad_buzzer c 264 0
-mknod /dev/csemad_push_switch c 265 0
+sudo rmmod led_driver
+sudo rmmod seven_segment_driver
+sudo rmmod dot_matrix_driver
+sudo rmmod lcd_text_driver
+sudo rmmod buzzer_driver
+sudo rmmod push_switch_driver
 
-rmmod led_driver
-rmmod seven_segment_driver
-rmmod dot_matrix_driver
-rmmod lcd_text_driver
-rmmod buzzer_driver
-rmmod push_switch_driver
+sudo insmod ./${DRIVER_DIR}/led_driver.ko
+sudo insmod ./${DRIVER_DIR}/seven_segment_driver.ko
+sudo insmod ./${DRIVER_DIR}/dot_matrix_driver.ko
+sudo insmod ./${DRIVER_DIR}/lcd_text_driver.ko
+sudo insmod ./${DRIVER_DIR}/buzzer_driver.ko
+sudo insmod ./${DRIVER_DIR}/push_switch_driver.ko
 
-insmod ./${DRIVER_DIR}/led_driver.ko
-insmod ./${DRIVER_DIR}/seven_segment_driver.ko
-insmod ./${DRIVER_DIR}/dot_matrix_driver.ko
-insmod ./${DRIVER_DIR}/lcd_text_driver.ko
-insmod ./${DRIVER_DIR}/buzzer_driver.ko
-insmod ./${DRIVER_DIR}/push_switch_driver_driver.ko
-
-lsmod
-
-exit
+sudo lsmod
