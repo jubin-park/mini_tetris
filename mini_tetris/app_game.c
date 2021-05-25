1bf2657b40d22d2a0b3ea59c91676a3119462f3d
jubin-park <jubin-park@naver.com>
2021-05-25 13:48:14 +0900

    try3

--- mini_tetris/app_game.c
+++ mini_tetris/app_game.c
@@ -47,9 +47,10 @@ int main(int argc, char* argv[])
         for (i = 0; i < DRIVER_SIZE; ++i) {
             has_error |= fd[i] < 0;
             if (fd[i] < 0) {
-                fprintf(stderr, "Failed to open driver: '%s'\n", DRIVER_NAMES[i])
-            } else {
-                printf("Succeed to open driver: '%s'\n", DRIVER_NAMES[i])
+                fprintf(stderr, "Failed to open driver: '%s'\n", DRIVER_NAMES[i]);
+            }
+            else {
+                printf("Succeed to open driver: '%s'\n", DRIVER_NAMES[i]);
             }
         }
 
