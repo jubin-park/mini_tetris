387ab8b28fa3718cae61638664f5083782e7afc0
Jubin Park <jubin-park@naver.com>
2021-05-25 17:02:42 +0900

    update: app_game.c

--- mini_tetris/app_game.c
+++ mini_tetris/app_game.c
@@ -11,27 +11,10 @@
 #include <assert.h>
 
 #include "blocks.h"
+#include "driver.h"
+#include "switch_key.h"
 #include "dot10x7/font.h"
 #include "dot10x7/full.h"
-
-typedef enum {
-    DRIVER_LED,
-    DRIVER_SEVEN_SEGMENT,
-    DRIVER_DOT_MATRIX,
-    DRIVER_LCD_TEXT,
-    DRIVER_BUZZER,
-    DRIVER_PUSH_SWITCH,
-    DRIVER_SIZE
-} driver_t;
-
-const char* DRIVER_NAMES[DRIVER_SIZE] = {
-    "/dev/csemad_led",
-    "/dev/csemad_seven_segment",
-    "/dev/csemad_dot_matrix",
-    "/dev/csemad_lcd_text",
-    "/dev/csemad_buzzer",
-    "/dev/csemad_push_switch"
-};
 
 bool g_is_game_running = true;
 
@@ -41,19 +24,16 @@ void display_matrix(const int fd);
 int main(int argc, char* argv[])
 {
     int fd[DRIVER_SIZE] = { -1, -1, -1, -1, -1, -1 };
-    fd[DRIVER_LED] = open(DRIVER_NAMES[DRIVER_LED], O_RDWR);
-    fd[DRIVER_SEVEN_SEGMENT] = open(DRIVER_NAMES[DRIVER_SEVEN_SEGMENT], O_RDWR);
-    fd[DRIVER_DOT_MATRIX] = open(DRIVER_NAMES[DRIVER_DOT_MATRIX], O_RDWR);
-    fd[DRIVER_LCD_TEXT] = open(DRIVER_NAMES[DRIVER_LCD_TEXT], O_RDWR);
-    fd[DRIVER_BUZZER] = open(DRIVER_NAMES[DRIVER_BUZZER], O_RDWR);
-    fd[DRIVER_PUSH_SWITCH] = open(DRIVER_NAMES[DRIVER_PUSH_SWITCH], O_RDWR);
     {
-        size_t i;
         bool has_error = false;
+        size_t i;
 
         for (i = 0; i < DRIVER_SIZE; ++i) {
-            has_error |= fd[i] < 0;
             printf("Loading driver '%s' ... ", DRIVER_NAMES[i]);
+
+            fd[i] = open(DRIVER_NAMES[i], O_RDWR);
+            has_error |= fd[i] < 0;
+
             puts(fd[i] < 0 ? "FAILED" : "success");
         }
 
@@ -71,32 +51,44 @@ int main(int argc, char* argv[])
     };
     uint32_t game_frame = 0;
     unsigned char old_buffer[ROW_COUNT] = { 0 };
+    unsigned char switch_states[SWITCH_KEY_SIZE] = { 0 };
 
     while (g_is_game_running)
     {
-        unsigned char display_buffer[ROW_COUNT];
-        memcpy(display_buffer, old_buffer, ROW_COUNT * sizeof(unsigned char));
-
-        // TODO: draw block
-        unsigned char line[3] = {
-            (block1[ANGLE_0][0][0] << 2) | (block1[ANGLE_0][0][1] << 1) | block1[ANGLE_0][0][2],
-            (block1[ANGLE_0][1][0] << 2) | (block1[ANGLE_0][1][1] << 1) | block1[ANGLE_0][1][2],
-            (block1[ANGLE_0][2][0] << 2) | (block1[ANGLE_0][2][1] << 1) | block1[ANGLE_0][2][2]
-        };
-
-        display_buffer[now_block.y + 0] |= line[0] << (6 - now_block.x);
-        display_buffer[now_block.y + 1] |= line[1] << (6 - now_block.x);
-        display_buffer[now_block.y + 2] |= line[2] << (6 - now_block.x);
-
-        // draw display_buffer
-        if (write(fd[DRIVER_DOT_MATRIX], display_buffer, ROW_COUNT * sizeof(unsigned char)) < 0) {
-            fprintf(stderr, "write() error\n");
+        {// get switch key state
+            size_t i;
             
-            goto lb_exit;
+            read(fd[DRIVER_PUSH_SWITCH], switch_states, sizeof(switch_states));
+            for (i = 0; i < 3; ++i) {
+                printf("%3d %3d %3d\n", switch_states[i * 3], switch_states[i * 3 + 1], switch_states[i * 3 + 2]);
+            }
         }
-    
-        now_block.y++;
-        now_block.y %= 10;
+
+        {// draw display_buffer
+            unsigned char display_buffer[ROW_COUNT];
+            memcpy(display_buffer, old_buffer, ROW_COUNT * sizeof(unsigned char));
+
+            // get block lines
+            unsigned char line[3] = {
+                (block1[ANGLE_0][0][0] << 2) | (block1[ANGLE_0][0][1] << 1) | block1[ANGLE_0][0][2],
+                (block1[ANGLE_0][1][0] << 2) | (block1[ANGLE_0][1][1] << 1) | block1[ANGLE_0][1][2],
+                (block1[ANGLE_0][2][0] << 2) | (block1[ANGLE_0][2][1] << 1) | block1[ANGLE_0][2][2]
+            };
+            // draw block on display_buffer
+            display_buffer[now_block.y + 0] |= line[0] << (6 - now_block.x);
+            display_buffer[now_block.y + 1] |= line[1] << (6 - now_block.x);
+            display_buffer[now_block.y + 2] |= line[2] << (6 - now_block.x);
+
+            // real drawing
+            if (write(fd[DRIVER_DOT_MATRIX], display_buffer, ROW_COUNT * sizeof(unsigned char)) < 0) {
+                fprintf(stderr, "write() error\n");
+                
+                goto lb_exit;
+            }
+
+            now_block.y++;
+            now_block.y %= ROW_COUNT;
+        }
 
         printf("frame = %4d\n", ++game_frame);
         display_matrix(fd[DRIVER_DOT_MATRIX]);
