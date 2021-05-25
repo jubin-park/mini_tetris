61ed0ace9f6670070618bc09ead961ff26301675
YoungHun Go <42068110+younghoongo@users.noreply.github.com>
2021-05-25 15:11:10 +0900

    fix: blocks dimension

--- mini_tetris/blocks.h
+++ mini_tetris/blocks.h
@@ -3,100 +3,80 @@
 
 #include <stdint.h>
 
-static const uint8_t shape1_0[3][3] = {
+static const uint8_t block1[4][3][3] = {
+{   
     {1, 1, 1},
     {0, 1, 0},
-    {0, 0, 0}
-};
-
-static const uint8_t shape1_90[3][3] = {
+    {0, 0, 0}},
+{
     {0, 0, 1},
     {0, 1, 1},
-    {0, 0, 1}
-};
-
-static const uint8_t shape1_180[3][3] = {
+    {0, 0, 1}},
+{
     {0, 0, 0},
     {0, 1, 0},
-    {1, 1, 1}
-};
-
-static const uint8_t shape1_270[3][3] = {
+    {1, 1, 1}},
+{
     {1, 0, 0},
     {1, 1, 0},
-    {1, 0, 0}
+    {1, 0, 0}}
 };
 
-static const uint8_t shape2_0[3][3] = {
+static const uint8_t block2[4][3][3] = {
+{
     {1, 0, 0},
     {1, 1, 1},
-    {0, 0, 0}
-};
-
-static const uint8_t shape2_90[3][3] = {
-    {1, 1, 0},
-    {1, 0, 0},
-    {1, 0, 0}
-};
-
-static const uint8_t shape2_180[3][3] = {
+    {0, 0, 0}},
+{
+    {0, 1, 1},
+    {0, 1, 0},
+    {0, 1, 0}},
+{
+    {0, 0, 0},
     {1, 1, 1},
-    {0, 0, 1},
-    {0, 0, 0}
+    {0, 0, 1}},
+{
+    {0, 1, 0},
+    {0, 1, 0},
+    {1, 1, 0}}
 };
 
-static const uint8_t shape2_270[3][3] = {
-    {0, 0, 1},
-    {0, 0, 1},
-    {0, 1, 1}
-};
-
-static const uint8_t shape3_0[3][3] = {
+static const uint8_t block3[4][3][3] = {
+{
     {1, 1, 0},
     {0, 1, 1},
-    {0, 0, 0}
-};
-
-static const uint8_t shape3_90[3][3] = {
+    {0, 0, 0}},
+{
     {0, 0, 1},
     {0, 1, 1},
-    {0, 1, 0}
-};
-
-static const uint8_t shape3_180[3][3] = {
+    {0, 1, 0}},
+{
     {0, 0, 0},
     {1, 1, 0},
-    {0, 1, 1}
-};
-
-static const uint8_t shape3_270[3][3] = {
+    {0, 1, 1}},
+{
     {0, 1, 0},
     {1, 1, 0},
-    {1, 0, 0}
+    {1, 0, 0}}
 };
 
-static const uint8_t shape4_0[3][3] = {
+static const uint8_t block4[4][3][3] = {
+{
     {1, 1, 1},
     {0, 0, 0},
-    {0, 0, 0}
-};
-
-static const uint8_t shape4_90[3][3] = {
+    {0, 0, 0}},
+{
     {0, 0, 1},
     {0, 0, 1},
-    {0, 0, 1}
-};
-
-static const uint8_t shape4_180[3][3] = {
+    {0, 0, 1}},
+{
     {0, 0, 0},
     {0, 0, 0},
-    {1, 1, 1}
-};
-
-static const uint8_t shape4_270[3][3] = {
+    {1, 1, 1}},
+{
     {1, 0, 0},
     {1, 0, 0},
-    {1, 0, 0}
+    {1, 0, 0}}
 };
 
 #endif /* BLOCKS_H */
