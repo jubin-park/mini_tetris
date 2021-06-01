08d5b6cca9d9ac11f73a4e717f82dee0dbff2199
Jubin Park <jubin-park@naver.com>
2021-06-01 15:21:05 +0900

    add: scene

--- mini_tetris/app_game.c
+++ mini_tetris/app_game.c
@@ -14,6 +14,7 @@
 #include "config.h"
 #include "block.h"
 #include "driver.h"
+#include "scene.h"
 #include "switch_key.h"
 #include "dot10x7/font.h"
 #include "dot10x7/full.h"
@@ -23,12 +24,16 @@ extern uint8_t g_score_text[SEVEN_SEGMENT_DATA_LENGTH];
 extern uint8_t g_now_switch_states[SWITCH_KEY_SIZE];
 extern uint8_t g_old_switch_states[SWITCH_KEY_SIZE];
 
-bool g_is_game_running = true;
-uint32_t g_score;
+static bool s_is_game_running = true;
+static uint32_t s_score;
+static uint32_t s_frame_count;
+static scene_t s_now_scene = SCENE_INTRO;
 
 void signal_exit(int sig);
 void print_matrix(const uint8_t* screen_buffer);
 bool render_matrix_to_device(const uint8_t* screen_buffer);
+void update_scene_intro(void);
+void update_scene_game(void);
 
 int main(void)
 {
@@ -47,182 +52,32 @@ int main(void)
     // generate random seed
     srandom((unsigned int)time(NULL));
 
-    uint32_t frame_count = 0;
-    uint8_t old_screen_buffer[SCREEN_HEIGHT] = { 0 };
-    block_t now_block = generate_random_block();
-
+    // set timespec
     struct timespec ts_sleep = {
         .tv_sec = 0,
         .tv_nsec = DELAY_NANOSEC_PER_FRAME
     };
 
-    while (g_is_game_running) {
+    while (s_is_game_running) {
 
-        bool is_redrawing_needed = false;
+        switch (s_now_scene) {
+            case SCENE_INTRO:
+                update_scene_intro();
+                break;
 
-        {// get switch key state
-            if (read(get_driver_file_descriptor(DRIVER_PUSH_SWITCH), g_now_switch_states, sizeof(g_now_switch_states)) < 0) {
-                fprintf(stderr, "Failed to read switch key\n");
+            case SCENE_GAME:
+                update_scene_game();
+                break;
 
-                goto lb_exit;
-            }
+            case SCENE_GAME_OVER:
+                break;
+
+            default:
+                assert(false && "invalid scene_t");
+        }
         
-            if (is_switch_key_triggered(SWITCH_KEY_UP)) {
-                puts("UP");
-            }
-            else if (is_switch_key_triggered(SWITCH_KEY_DOWN)) {
-                puts("DOWN");
-                while (is_passable_down(old_screen_buffer, &now_block)) {
-                    ++now_block.y;
-                    is_redrawing_needed = true;
-                }
-            }
-            else if (is_switch_key_triggered(SWITCH_KEY_LEFT)) {
-                puts("LEFT");
-                if (is_passable_left(old_screen_buffer, &now_block)) {
-                    --now_block.x;
-                    is_redrawing_needed = true;
-                }
-            }
-            else if (is_switch_key_triggered(SWITCH_KEY_RIGHT)) {
-                puts("RIGHT");
-                if (is_passable_right(old_screen_buffer, &now_block)) {
-                    ++now_block.x;
-                    is_redrawing_needed = true;
-                }
-            }
-            else if (is_switch_key_triggered(SWITCH_KEY_OK_OR_ROTATE)) {
-                puts("ROTATE");
-                if (is_rotatable_clockwise(old_screen_buffer, &now_block)) {
-                    now_block.angle = (now_block.angle + 1) % ANGLE_SIZE;
-                    is_redrawing_needed = true;
-                }
-            }
-
-            memcpy(g_old_switch_states, g_now_switch_states, sizeof(g_now_switch_states));
-        }
-
-        if (is_redrawing_needed) { // is_redrawing_needed when block moved or rotated
-            printf("x: %2d\ty: %2d\n", now_block.x, now_block.y);
-
-            uint8_t new_screen_buffer[SCREEN_HEIGHT];
-            memcpy(new_screen_buffer, old_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
-
-            const uint8_t* p_block_tiles = now_block.tile_of_zero_angle + (now_block.angle * BLOCK_WIDTH * BLOCK_HEIGHT);
-
-            // draw block on new_screen_buffer
-            for (int y = 0; y < BLOCK_HEIGHT; ++y) {
-                if (now_block.y + y >= 0 && now_block.y + y < SCREEN_HEIGHT) {
-                    uint8_t line = (p_block_tiles + y * BLOCK_WIDTH)[2] << 2 | (p_block_tiles + y * BLOCK_WIDTH)[1] << 1 | (p_block_tiles + y * BLOCK_WIDTH)[0];
-                    if (now_block.x >= 0) {
-                        line <<= now_block.x;
-                    } else {
-                        line >>= (-now_block.x);
-                    }
-                    new_screen_buffer[now_block.y + y] |= line;
-                }
-            }
-
-            // real drawing
-            if (!render_matrix_to_device(new_screen_buffer)) {
-                fprintf(stderr, "render_matrix_to_device() error\n");
-
-                goto lb_exit;
-            }
-        }
-
-        // draw new_screen_buffer per a frame
-        if (0 == frame_count % DRAWING_DELAY_FRAME_COUNT) {
-            uint8_t new_screen_buffer[SCREEN_HEIGHT];
-            memcpy(new_screen_buffer, old_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
-
-            const uint8_t* p_block_tiles = now_block.tile_of_zero_angle + (now_block.angle * BLOCK_WIDTH * BLOCK_HEIGHT);
-            printf("block #%d\tx: %2d y: %2d\tangle: %3d\tframe: %6d\n",
-                (now_block.tile_of_zero_angle - BLOCK_TILES[0]) / (BLOCK_HEIGHT * ANGLE_SIZE * BLOCK_WIDTH),
-                now_block.x,
-                now_block.y,
-                now_block.angle * 90,
-                frame_count);
-
-            // draw block on new_screen_buffer
-            for (int y = 0; y < BLOCK_HEIGHT; ++y) {
-                if (now_block.y + y >= 0 && now_block.y + y < SCREEN_HEIGHT) {
-                    uint8_t line = (p_block_tiles + y * BLOCK_WIDTH)[2] << 2 | (p_block_tiles + y * BLOCK_WIDTH)[1] << 1 | (p_block_tiles + y * BLOCK_WIDTH)[0];
-                    if (now_block.x >= 0) {
-                        line <<= now_block.x;
-                    } else {
-                        line >>= (-now_block.x);
-                    }
-                    new_screen_buffer[now_block.y + y] |= line;
-                }
-            }
-
-            // real drawing
-            if (!render_matrix_to_device(new_screen_buffer)) {
-                fprintf(stderr, "render_matrix_to_device() error\n");
-
-                goto lb_exit;
-            }
-
-            if (!is_passable_down(new_screen_buffer, &now_block)) {
-                printf("collision occured\n");
-
-                if ((new_screen_buffer[0] & 0x7f) > 0) {
-                    puts("== GAME OVER ==");
-                    goto lb_exit;
-                }
-
-                int8_t removed_height = 0;
-                
-                while (true) {
-                    bool is_line_found = false;
-
-                    for (int8_t h = SCREEN_HEIGHT - 1; h >= 0; --h) {
-                        if ((new_screen_buffer[h] & 0xff) == 0x7f) {
-                            ++removed_height;
-                            
-                            for (int8_t y = h; y >= 1; --y) {
-                                new_screen_buffer[y] = new_screen_buffer[y - 1];
-                            }
-
-                            is_line_found = true;
-
-                            break;
-                        }
-                    }
-
-                    if (!is_line_found) {
-                        break;
-                    }
-                }
-
-                if (removed_height > 0) {
-                    memset(new_screen_buffer, 0x0, removed_height * sizeof(uint8_t));
-                    g_score += removed_height * DEFAULT_SCORE;
-
-                    update_score_text(g_score);
-
-                    // when line is removed, is_redrawing_needed again
-                    if (!render_matrix_to_device(new_screen_buffer)) {
-                        fprintf(stderr, "render_matrix_to_device() error\n");
-
-                        goto lb_exit;
-                    }
-                }
-
-                memcpy(old_screen_buffer, new_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
-
-                now_block = generate_random_block();
-            }
-            else {
-                now_block.y++;
-            }
-
-            print_matrix(new_screen_buffer);
-        }
-
         nanosleep(&ts_sleep, NULL);
-        ++frame_count;
+        ++s_frame_count;
     }
 
 lb_exit:
@@ -237,7 +92,7 @@ lb_exit:
 void signal_exit(int sig)
 {
     printf("<Exit Game>\tsig: %d\n", sig);
-    g_is_game_running = false;
+    s_is_game_running = false;
 }
 
 void print_matrix(const uint8_t* screen_buffer)
@@ -252,16 +107,196 @@ void print_matrix(const uint8_t* screen_buffer)
 
 bool render_matrix_to_device(const uint8_t* screen_buffer)
 {
-    uint8_t flip_buffer[SCREEN_HEIGHT];
+    uint8_t reversed_buffer[SCREEN_HEIGHT];
     
     for (int8_t y = 0; y < SCREEN_HEIGHT; ++y) {
         const uint8_t bits = screen_buffer[y];
 
-        flip_buffer[y] = (bits & 0x1) << 6 | (bits & 0x40) >> 6
+        reversed_buffer[y] = (bits & 0x1) << 6 | (bits & 0x40) >> 6
                         | (bits & 0x2) << 4 | (bits & 0x20) >> 4
                         | (bits & 0x4) << 2 | (bits & 0x10) >> 2
                         | (bits & 0x8);
     }
 
-    return write(get_driver_file_descriptor(DRIVER_DOT_MATRIX), flip_buffer, SCREEN_HEIGHT * sizeof(uint8_t)) >= 0;
+    return write(get_driver_file_descriptor(DRIVER_DOT_MATRIX), reversed_buffer, SCREEN_HEIGHT * sizeof(uint8_t)) >= 0;
+}
+
+void update_scene_intro(void)
+{
+
+}
+
+void update_scene_game(void)
+{
+    bool is_redrawing_needed = false;
+    uint8_t old_screen_buffer[SCREEN_HEIGHT] = { 0 };
+    block_t now_block = generate_random_block();
+
+    {// get switch key state
+        if (read(get_driver_file_descriptor(DRIVER_PUSH_SWITCH), g_now_switch_states, sizeof(g_now_switch_states)) < 0) {
+            fprintf(stderr, "Failed to read switch key\n");
+
+            goto lb_exit;
+        }
+
+        if (is_switch_key_triggered(SWITCH_KEY_UP)) {
+            puts("UP");
+        }
+        else if (is_switch_key_triggered(SWITCH_KEY_DOWN)) {
+            puts("DOWN");
+            while (is_passable_down(old_screen_buffer, &now_block)) {
+                ++now_block.y;
+                
+                is_redrawing_needed = true;
+            }
+        }
+        else if (is_switch_key_triggered(SWITCH_KEY_LEFT)) {
+            puts("LEFT");
+            if (is_passable_left(old_screen_buffer, &now_block)) {
+                --now_block.x;
+
+                is_redrawing_needed = true;
+            }
+        }
+        else if (is_switch_key_triggered(SWITCH_KEY_RIGHT)) {
+            puts("RIGHT");
+            if (is_passable_right(old_screen_buffer, &now_block)) {
+                ++now_block.x;
+
+                is_redrawing_needed = true;
+            }
+        }
+        else if (is_switch_key_triggered(SWITCH_KEY_OK_OR_ROTATE)) {
+            puts("ROTATE");
+            if (is_rotatable_clockwise(old_screen_buffer, &now_block)) {
+                now_block.angle = (now_block.angle + 1) % ANGLE_SIZE;
+
+                is_redrawing_needed = true;
+            }
+        }
+
+        memcpy(g_old_switch_states, g_now_switch_states, sizeof(g_now_switch_states));
+    }
+
+    if (is_redrawing_needed) { // is_redrawing_needed when block moved or rotated
+        printf("x: %2d\ty: %2d\n", now_block.x, now_block.y);
+
+        uint8_t new_screen_buffer[SCREEN_HEIGHT];
+        memcpy(new_screen_buffer, old_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
+
+        const uint8_t* p_block_tiles = now_block.tile_of_zero_angle + (now_block.angle * BLOCK_WIDTH * BLOCK_HEIGHT);
+
+        // draw block on new_screen_buffer
+        for (int y = 0; y < BLOCK_HEIGHT; ++y) {
+            if (now_block.y + y >= 0 && now_block.y + y < SCREEN_HEIGHT) {
+                uint8_t line = (p_block_tiles + y * BLOCK_WIDTH)[2] << 2 | (p_block_tiles + y * BLOCK_WIDTH)[1] << 1 | (p_block_tiles + y * BLOCK_WIDTH)[0];
+                if (now_block.x >= 0) {
+                    line <<= now_block.x;
+                } else {
+                    line >>= (-now_block.x);
+                }
+                new_screen_buffer[now_block.y + y] |= line;
+            }
+        }
+
+        // real drawing
+        if (!render_matrix_to_device(new_screen_buffer)) {
+            fprintf(stderr, "render_matrix_to_device() error\n");
+
+            goto lb_exit;
+        }
+    }
+
+    // draw new_screen_buffer per a frame
+    if (0 == s_frame_count % DRAWING_DELAY_FRAME_COUNT) {
+        uint8_t new_screen_buffer[SCREEN_HEIGHT];
+        memcpy(new_screen_buffer, old_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
+
+        const uint8_t* p_block_tiles = now_block.tile_of_zero_angle + (now_block.angle * BLOCK_WIDTH * BLOCK_HEIGHT);
+        printf("block #%d\tx: %2d y: %2d\tangle: %3d\tframe: %6d\n",
+            (now_block.tile_of_zero_angle - BLOCK_TILES[0]) / (BLOCK_HEIGHT * ANGLE_SIZE * BLOCK_WIDTH),
+            now_block.x,
+            now_block.y,
+            now_block.angle * 90,
+            s_frame_count);
+
+        // draw block on new_screen_buffer
+        for (int y = 0; y < BLOCK_HEIGHT; ++y) {
+            if (now_block.y + y >= 0 && now_block.y + y < SCREEN_HEIGHT) {
+                uint8_t line = (p_block_tiles + y * BLOCK_WIDTH)[2] << 2 | (p_block_tiles + y * BLOCK_WIDTH)[1] << 1 | (p_block_tiles + y * BLOCK_WIDTH)[0];
+                if (now_block.x >= 0) {
+                    line <<= now_block.x;
+                } else {
+                    line >>= (-now_block.x);
+                }
+                new_screen_buffer[now_block.y + y] |= line;
+            }
+        }
+
+        // real drawing
+        if (!render_matrix_to_device(new_screen_buffer)) {
+            fprintf(stderr, "render_matrix_to_device() error\n");
+
+            goto lb_exit;
+        }
+
+        if (!is_passable_down(new_screen_buffer, &now_block)) {
+            printf("collision occured\n");
+
+            if ((new_screen_buffer[0] & 0x7f) > 0) {
+                puts("== GAME OVER ==");
+                goto lb_exit;
+            }
+
+            int8_t removed_height = 0;
+            
+            while (true) {
+                bool is_line_found = false;
+
+                for (int8_t h = SCREEN_HEIGHT - 1; h >= 0; --h) {
+                    if ((new_screen_buffer[h] & 0xff) == 0x7f) {
+                        ++removed_height;
+                        
+                        for (int8_t y = h; y >= 1; --y) {
+                            new_screen_buffer[y] = new_screen_buffer[y - 1];
+                        }
+
+                        is_line_found = true;
+
+                        break;
+                    }
+                }
+
+                if (!is_line_found) {
+                    break;
+                }
+            }
+
+            if (removed_height > 0) {
+                memset(new_screen_buffer, 0x0, removed_height * sizeof(uint8_t));
+                s_score += removed_height * DEFAULT_SCORE;
+
+                update_score_text(s_score);
+
+                // when line is removed, is_redrawing_needed again
+                if (!render_matrix_to_device(new_screen_buffer)) {
+                    fprintf(stderr, "render_matrix_to_device() error\n");
+
+                    goto lb_exit;
+                }
+            }
+
+            memcpy(old_screen_buffer, new_screen_buffer, SCREEN_HEIGHT * sizeof(uint8_t));
+
+            now_block = generate_random_block();
+        }
+        else {
+            now_block.y++;
+        }
+
+        print_matrix(new_screen_buffer);
+    }
+
+lb_exit:
+    return;
 }
---
+++ mini_tetris/scene.h
@@ -0,0 +1,13 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+typedef enum {
+    SCENE_INTRO,
+    SCENE_GAME,
+    SCENE_GAME_OVER,
+
+    /* SIZE */
+    SCENE_SIZE
+} scene_t;
+
+#endif
