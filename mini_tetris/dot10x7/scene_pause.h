8e6135c1257e97fd4865ead0d35324e3dcbd2f5d
Jubin Park <jubin-park@naver.com>
2021-06-01 17:38:41 +0900

    update2

--- mini_tetris/dot10x7/scene_pause.h
+++ mini_tetris/dot10x7/scene_pause.h
@@ -1,10 +1,10 @@
 #include <stdint.h>
 
-#define SCENE_PAUSE_FRAME_COUNT 2
+#define SCENE_PAUSE_FRAME_COUNT 4
 
 /* Piskel data for "scene_pause" */
 
-static const uint8_t scene_pause_data[2][10] = {
+static const uint8_t scene_pause_data[4][10] = {
 {
 0x00,
 0x00,
@@ -16,6 +16,30 @@ static const uint8_t scene_pause_data[2][10] = {
 0x36,
 0x00,
 0x00
+},
+{
+0x00,
+0x00,
+0x36,
+0x36,
+0x36,
+0x36,
+0x36,
+0x36,
+0x00,
+0x00
+},
+{
+0x7f,
+0x7f,
+0x49,
+0x49,
+0x49,
+0x49,
+0x49,
+0x49,
+0x7f,
+0x7f
 },
 {
 0x7f,
