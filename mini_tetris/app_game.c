bbf6fdb5afb4e6ee83974c23f9b3ba2635a6cc2f
Jubin Park <jubin-park@naver.com>
2021-06-01 17:23:49 +0900

    update

--- mini_tetris/app_game.c
+++ mini_tetris/app_game.c
@@ -80,11 +80,11 @@ int main(void)
                 break;
 
             case SCENE_PAUSE:
-
+                update_scene_pause();
                 break;
 
             case SCENE_GAME_OVER:
-
+                update_scene_gameover();
                 break;
 
             default:
@@ -211,7 +211,7 @@ void update_scene_game(void)
                     || is_switch_key_triggered(SWITCH_KEY_2)
                     || is_switch_key_triggered(SWITCH_KEY_6)
                     || is_switch_key_triggered(SWITCH_KEY_8)) {
-
+            puts("PAUSED");
             s_now_scene = SCENE_PAUSE;
 
             goto lb_exit;
@@ -349,8 +349,6 @@ void update_scene_pause(void)
 
     write(get_driver_file_descriptor(DRIVER_DOT_MATRIX), scene_pause_data[s_scene_frame], SCREEN_HEIGHT * sizeof(uint8_t));
     s_scene_frame = (s_scene_frame + 1) % SCENE_PAUSE_FRAME_COUNT;
-    
-    printf("PAUSED\n");
 
     {// get switch key state
         if (read(get_driver_file_descriptor(DRIVER_PUSH_SWITCH), g_now_switch_states, sizeof(g_now_switch_states)) < 0) {
